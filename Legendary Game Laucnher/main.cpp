014f9336d4a049615270ff0e727cccbae3ee66aa
Ethan James <lskywalker416@gmail.com>
2023-04-16 13:00:02 -0600

    commands now work

--- Legendary Game Laucnher/main.cpp
+++ Legendary Game Laucnher/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
-#include <windows.h>
 #include <boost/program_options.hpp>
+#include <windows.h>
+#include <locale>
+#include <codecvt>
 
 int main(int argc, char *argv[])
 {
@@ -31,23 +33,92 @@ int main(int argc, char *argv[])
 
     if (vm.count("game")) {
         std::cout << "Game id was set to " << vm["gameid"].as<int>() << std::endl;
+        std::cout << "Launching game..." << std::endl;
+
+        std::string command = "C:\\Users\\sansm\\bin\\legendary.exe launch " + vm["gameid"].as<int>();
+
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        std::wstring wstr = converter.from_bytes(command);
+
+
+        STARTUPINFOW si = { 0 };
+        si.cb = sizeof(STARTUPINFOW);
+        PROCESS_INFORMATION pi = { 0 };
+
+        BOOL success = CreateProcessW(NULL, const_cast<wchar_t*>(wstr.c_str()), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+        if (!success) {
+            std::cerr << "Failed to start process. Error code: " << GetLastError() << std::endl;
+            system("pause");
+            return 1;
+        }
+
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        DWORD exitCode = 0;
+        if (!GetExitCodeProcess(pi.hProcess, &exitCode)) {
+            std::cerr << "Failed to get process exit code. Error code: " << GetLastError() << std::endl;
+            system("pause");
+        }
+        else {
+            std::cout << "Process exited with code " << exitCode << std::endl;
+        }
+
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        
     }
     else {
-		std::cout << "Game id was not set. Do you want to set it now?" << std::endl;
+		std::cout << "Game id was not set. Do you want to set it now? [Y/n]" << std::endl;
         std::string input;
         std::cin >> input;
         if (input == "yes" || input == "y") {
-			std::cout << "Enter game id: ";
-			std::cin >> input;
-			std::cout << "Game id was set to " << input << std::endl;
+            std::cout << "Enter game id: ";
+            std::cin >> input;
+            std::cout << "Game id was set to " << input << std::endl;
+            std::cout << "Launching game..." << std::endl;
+
+            std::string command = "C:\\Users\\sansm\\bin\\legendary.exe launch " + input;
+
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+            std::wstring wstr = converter.from_bytes(command);
+
+
+            STARTUPINFOW si = { 0 };
+            si.cb = sizeof(STARTUPINFOW);
+            PROCESS_INFORMATION pi = { 0 };
+
+            BOOL success = CreateProcessW(NULL, const_cast<wchar_t*>(wstr.c_str()), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+            if (!success) {
+                std::cerr << "Failed to start process. Error code: " << GetLastError() << std::endl;
+                system("pause");
+                return 1;
+            }
+
+            WaitForSingleObject(pi.hProcess, INFINITE);
+
+            DWORD exitCode = 0;
+            if (!GetExitCodeProcess(pi.hProcess, &exitCode)) {
+                std::cerr << "Failed to get process exit code. Error code: " << GetLastError() << std::endl;
+                system("pause");
+            }
+            else {
+                std::cout << "Process exited with code " << exitCode << std::endl;
+            }
+
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+     
+            
 		}
         else {
 			std::cout << "Game id was not set. Exiting..." << std::endl;
-			return 1;
+            system("pause");
+			return 1;   
         }
 
 	}
 
-    std::cout << "Hello World!\n";
+    std::cout << "Finished all tasks. Exiting..." << std::endl;
+    system("pause");
+    return 0;
 }
-
